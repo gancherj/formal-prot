5c6a59cd3d5aaaf8a91d11fe90b04ede2bafc65c
gancherj <jrg358@cornell.edu>
2018-02-14 17:21:15 -0500

    move to dep record

--- coq/coalg.v
+++ coq/coalg.v
@@ -3,6 +3,7 @@ Require Import Arith.
 Require Import Ascii.
 Require Import String.
 Require Import List.
+Require Import Permutation.
 
 Definition Id := string.
 Definition eq_Id := string_dec.
@@ -18,50 +19,62 @@ Module Type LANG.
   Parameter denote : Com -> (Id -> Val) -> (PID -> Val) -> (Id -> Val) * (PID -> Val).
 End LANG.
 
-(* TODO: allow for a single party to stand in for multiple PIDs. *)
-Module Type MPSParties (L : LANG).
-  Parameter Parties : list (PID * L.Com).
-  Parameter wf_Parties : forall p1 p2, In p1 Parties -> In p2 Parties -> p1 <> p2 -> (fst p1) <> (fst p2).
-  Parameter Adv_Id : PID.
-End MPSParties.
+Record Lang := mkLang {
+  Val : Type;
+  Com : Type;
+  denote : Com -> (Id -> Val) -> (PID -> Val) -> (Id -> Val) * (PID -> Val)
+}.
 
-Module MPS (L : LANG) (P : MPSParties L).
 
-  Definition GSt := PID -> (Id -> L.Val).
-  Definition update_Gst (i : PID) (s : Id -> L.Val) (gs : GSt) := fun j => if eq_PID j i then s else gs j.
+
+Record Party {L : Lang} {pids : list PID} := mkParty {
+  parties : list (PID * (Com L));
+  adv_id : PID;
+  wf_parties : forall p1 p2, In p1 parties -> In p2 parties -> p1 <> p2 -> (fst p1) <> (fst p2)
+               /\ Permutation pids (adv_id :: (map fst parties))
+  }.
+  
+
+Section MPS.
+  Hypothesis L : Lang.
+  Hypothesis pids : list PID.
+  Hypothesis P : @Party L pids.
+
+  Definition GSt := PID -> (Id -> (Val L)).
+  Definition update_Gst (i : PID) (s : Id -> (Val L)) (gs : GSt) := fun j => if eq_PID j i then s else gs j.
 
 
 (* GB i j = messages from i to j *)
-  Definition GBuf := PID -> PID -> L.Val.
+  Definition GBuf := PID -> PID -> (Val L).
   (* messages to i *)
-  Definition ins (i : PID) (g : GBuf) : PID -> L.Val := fun j => g j i.
+  Definition ins (i : PID) (g : GBuf) : PID -> (Val L) := fun j => g j i.
   (* messages from i *)
-  Definition outs (i : PID) (g : GBuf) : PID -> L.Val := fun j => g i j.
+  Definition outs (i : PID) (g : GBuf) : PID -> (Val L) := fun j => g i j.
 
-  Definition GTrans (gst : GSt) (buf : GBuf) (c : PID * L.Com) : GSt * GBuf := 
-    let p := L.denote (snd c) (gst (fst c)) (ins (fst c) buf) in
+  Definition GTrans (gst : GSt) (buf : GBuf) (c : PID * (Com L)) : GSt * GBuf := 
+    let p := (denote L) (snd c) (gst (fst c)) (ins (fst c) buf) in
     (update_Gst (fst c) (fst p) gst, fun i' j => if eq_PID i' (fst c) then (snd p) j else buf i' j).
 
-  Definition round_robin (g : GSt * GBuf) (cs : list (PID * L.Com)) : GSt * GBuf :=
+  Definition round_robin (g : GSt * GBuf) (cs : list (PID * (Com L))) : GSt * GBuf :=
     fold_left (fun acc c => GTrans (fst acc) (snd acc) c) cs g.
+  
 
-  Definition C_tr (g : GSt * GBuf) (c : L.Com) : GSt * GBuf := round_robin g ((P.Adv_Id, c) :: P.Parties).
+  Definition C_tr (g : GSt * GBuf) (c : (Com L)) : GSt * GBuf := round_robin g ((adv_id P, c) :: (parties P)).
 
   Definition adv_Bisim_prog (R : GSt * GBuf -> GSt * GBuf -> Prop) : GSt * GBuf -> GSt * GBuf -> Prop :=
   fun p1 p2 => R p1 p2 -> 
     forall com,
     let g1 := C_tr p1 com in
     let g2 := C_tr p2 com in
-    R g1 g2 /\ (forall s, (fst g1) P.Adv_Id s = (fst g2) P.Adv_Id s) /\ (forall j, (snd g1) j P.Adv_Id = (snd g2) j P.Adv_Id).
+    R g1 g2 /\ (forall s, (fst g1) (adv_id P) s = (fst g2) (adv_id P) s) /\ (forall j, (snd g1) j (adv_id P) = (snd g2) j (adv_id P)).
 
   Definition adv_st_rel (g1 : GSt * GBuf) (g2 : GSt * GBuf) : Prop :=
-    (forall s, (fst g1) P.Adv_Id s = (fst g2) P.Adv_Id s) /\ (forall j, (snd g1) j P.Adv_Id = (snd g2) j P.Adv_Id).
+    (forall s, (fst g1) (adv_id P) s = (fst g2) (adv_id P) s) /\ (forall j, (snd g1) j (adv_id P) = (snd g2) j (adv_id P)).
 
   Definition adv_Bisim :=
   forall g1 g2, adv_st_rel g1 g2 -> (adv_Bisim_prog adv_st_rel) g1 g2.
 
 End MPS.
 
-(* TODO: define relational variant of MPS. *)
 
 
